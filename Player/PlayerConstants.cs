691cff4699f77a273aba65fa14ee82ff8f673307
bjarkiella <bjarkiella@gmail.com>
2025-07-26 19:51:59 +0200

    movement working fine now

--- Player/PlayerConstants.cs
+++ Player/PlayerConstants.cs
@@ -7,11 +7,15 @@ using System.Threading.Tasks;
 namespace Boids
 {
     public static class PlayerConstants
-{
+    {
         public static float visionFactor = 1f;
         public static float maxSpeed = 240f;
         public static float maxAccel = 100f;
-        public static float drag = 2f;
+        public static float drag = 100f;
         public static float maxTurn = MathF.PI * 0.95f;
+        public static float sprintTime = 1f;
+        public static float sprintAcc = 2f;
+        public static float sprintSpeed = 2f;
+        public static float sprintCoolDown = 5f;
     }
 }
--- Player/PlayerEntity.cs
+++ Player/PlayerEntity.cs
@@ -12,18 +12,22 @@ using Microsoft.Xna.Framework.Input;
 // TODO: Change this class so it makes sense the player uses it
 namespace Boids
 {
-    internal class PlayerEntity:BaseEntity
+    internal class PlayerEntity : BaseEntity
     {
         private Vector2 _heading = Vector2.Zero;
         private float _speed = 0f;
-
+        private float _sprintTimeLeft = 0f;
+        private float _coolDown = 0f;
+        private bool _sprinting = false;
+        private float _sprintAcc = 1f;
+        private float _sprintSpeed = 1f;
         public PlayerEntity(Texture2D texture, Vector2 position, Vector2 velocity, float visionFactor) : base(texture, position, velocity, visionFactor)
         {
 
         }
         public void Update(GameTime gameTime, KeyboardState current, KeyboardState _prevKeyboardState)
         {
-            float dt = Utils.deltaTime(gameTime) ;
+            float dt = Utils.deltaTime(gameTime);
             bool edge = BoundaryCond.EdgeCheck(Position, Radius);
 
             // Keyboard inputs for player
@@ -45,12 +49,38 @@ namespace Boids
             {
                 move.X -= 1;
             }
-            if (current.IsKeyDown(Keys.Space) && !_prevKeyboardState.IsKeyDown(Keys.Space))
+            if ((current.IsKeyDown(Keys.LeftShift) && !_prevKeyboardState.IsKeyDown(Keys.LeftShift)) &&
+            !_sprinting &&
+            _coolDown <= 0f &&
+            move.Length() > 0)
             {
-
-
+                _sprinting = true;
+                _sprintTimeLeft = PlayerConstants.sprintTime;
+                _sprintAcc = PlayerConstants.sprintAcc;
+                _sprintSpeed = PlayerConstants.sprintSpeed;
             }
 
+            // Sprinting conditions 
+            if (_sprinting)
+            {
+                _sprintTimeLeft -= dt;
+                _sprintTimeLeft = MathF.Max(0f, _sprintTimeLeft);
+                if (_sprintTimeLeft <= 0f)
+                {
+                    _sprinting = false;
+                    _sprintTimeLeft = 0f;
+                    _sprintAcc = 1f;
+                    _sprintSpeed = 1f;
+                    _coolDown = PlayerConstants.sprintCoolDown;
+                }
+            }
+            else if (_coolDown >= 0f)
+            {
+                _coolDown -= dt;
+                _coolDown = MathF.Max(0f, _coolDown);
+            }
+
+            // Movement of player set 
             if (move != Vector2.Zero)
             {
                 // Smoothing out the heading (same as in steer in boidmanager)
@@ -58,15 +88,15 @@ namespace Boids
                 float currentAngle = MathF.Atan2(_heading.Y, _heading.X);
                 float desiredAngle = MathF.Atan2(heading.Y, heading.X);
                 float rawDelta = desiredAngle - currentAngle;
-                float angleDiff = MathHelper.WrapAngle(rawDelta); 
+                float angleDiff = MathHelper.WrapAngle(rawDelta);
                 float maxTurn = PlayerConstants.maxTurn * dt;
                 float turn = MathHelper.Clamp(angleDiff, -maxTurn, maxTurn);
-                float newAngle = currentAngle + turn; 
+                float newAngle = currentAngle + turn;
                 _heading = new Vector2(MathF.Cos(newAngle), MathF.Sin(newAngle));
 
                 // Smoothing out the speed
-                _speed += PlayerConstants.maxAccel * dt;
-                _speed = MathF.Min(_speed, PlayerConstants.maxSpeed);
+                _speed += PlayerConstants.maxAccel * _sprintAcc * dt;
+                _speed = MathF.Min(_speed, PlayerConstants.maxSpeed) * _sprintSpeed;
             }
 
             else if (MathF.Round(Velocity.Length()) < 1e-3 || edge)
@@ -75,14 +105,20 @@ namespace Boids
             }
             else
             {
-                //Velocity *= 1f - PlayerConstants.drag * dt;
                 _speed -= PlayerConstants.drag * dt;
                 _speed = MathF.Max(_speed, 0f);
             }
             Velocity = _heading * _speed;
             Position += Velocity * dt;
             Position = BoundaryCond.PosCheck(Position, Radius);
-            Console.WriteLine(_speed);
+        }
+        private void sprint()
+        {
+
+        }
+        private void moveFrame()
+        {
+
         }
         public void Draw(SpriteBatch sb)
         {
